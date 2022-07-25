629e569b6f86003aad18794d21ea1c1d48a37463
Will Gauvin <wgauvin@swin.edu.au>
2022-07-25 15:30:27 +1000

    AT3-223 fix linting issue

--- src/ska_pst_lmc/smrb/smrb_model.py
+++ src/ska_pst_lmc/smrb/smrb_model.py
@@ -12,6 +12,7 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Dict, List
 
+
 @dataclass
 class SmrbMonitorDataStore:
     """Data store use to aggregate the subband data.
