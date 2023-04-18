0e9cf603edef57796717beb4bf80cb36cd279f51
Will Gauvin <wgauvin@swin.edu.au>
2023-04-18 11:00:22 +1000

    AT3-425 add fixture annotation on monitor_polling_rate_ms

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -7,7 +7,10 @@
 
 """This module defines fixtures specifically for the integration tests."""
 
+import pytest
 
+
+@pytest.fixture
 def monitor_polling_rate_ms() -> int:
     """Get monitor polling rate in milliseconds."""
     return 500
