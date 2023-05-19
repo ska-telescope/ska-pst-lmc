2383e5b8eaa077de8f2f44177a4b4a65dd971df4
Will Gauvin <wgauvin@swin.edu.au>
2023-05-19 11:58:13 +1000

    AT3-390 remove diskCapacity from attribute assertion

--- tests/unit/test_beam_device.py
+++ tests/unit/test_beam_device.py
@@ -145,7 +145,7 @@ class TestPstBeam:
         return {
             attr: getattr(self.beam_proxy, attr)
             for attr in self._beam_attribute_names
-            if attr != "availableDiskSpace"
+            if attr not in ["availableDiskSpace", "diskCapacity"]
         }
 
     @backoff.on_exception(
